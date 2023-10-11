ca98429751aa24c235a32182b62c9f8c3c22da3d
Arunarchitect <intformbase@gmail.com>
2023-10-11 14:41:01 +0530

    added navbar

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import SideNav from './components/SideNav'
+
 import Main from './components/Main'
-import Donate from './components/Donate'
 
 import 'vite/modulepreload-polyfill'
 
 function App() {
 
   return (
-    <div>
-      <SideNav />
+    <div className='App'>
       <Main />
-      <Donate />
     </div>
   )
 }
