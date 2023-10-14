bffa1a700cbb1035b9a2973b6bee18e02c572599
Arunarchitect <intformbase@gmail.com>
2023-10-14 11:32:29 +0530

    tried to post data  but failed

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class React(models.Model):
-    employee = models.CharField(max_length=30)
-    department = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=200, null=True)
+    password = models.CharField(max_length=20, null=True)
 
     class Meta:
         verbose_name_plural = "React"
 
     def __str__(self):
-        return self.employee
+        return self.full_name
--- frontend/frontend/src/components/User/Register.jsx
+++ frontend/frontend/src/components/User/Register.jsx
@@ -1,6 +1,40 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
+const baseUrl= 'http://127.0.0.1:8000/api/react/';
 function Register(){
+    const [personData, setpersonData] = useState({
+        'full_name': '',
+        'email': '',
+        'password': ''
+    })
+    
+    // change element value
+    const handleChange=(event)=>{
+        setpersonData({
+            ...personData,
+            [event.target.name]:event.target.value
+        })
+    }
+    // submit form
+    const submitForm = async () => {
+        const personFormData = new FormData();
+        personFormData.append('full_name', personData.full_name);
+        personFormData.append('email', personData.email);
+        personFormData.append('password', personData.password);
+    
+        try {
+            const response = await axios.post(baseUrl, personFormData);
+            console.log(response.data);
+        } catch (error) {
+            console.error("Error in registration:", error);
+        }
+    }
+    useEffect(()=>{
+        console.log(personData);
+        document.title='Register';
+    })
     return(
         <div className="container mt-4">
             <div className="row justify-content-center"> {/* Added 'justify-content-center' class */}
@@ -11,25 +45,17 @@ function Register(){
                     <form>
                         <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Full Name</label>
-                        <input type="email" className="form-control" />
+                        <input onChange={handleChange}  name="full_name" type="text" className="form-control" />
                         </div>
                         <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Email</label>
-                        <input type="password" className="form-control" />
-                        </div>
-                        <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Username</label>
-                        <input type="password" className="form-control" />
+                        <input onChange={handleChange}  name="email" type="email" className="form-control" />
                         </div>
                         <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" />
+                        <input onChange={handleChange} name="password" type="password" className="form-control" />
                         </div>
-                        <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Remember Me</label>
-                        </div>
-                        <button type="submit" className="btn btn-primary">Login</button>
+                        <button onClick={submitForm} type="submit" className="btn btn-primary">Register</button>
                     </form>
                     </div>
                 </div>
