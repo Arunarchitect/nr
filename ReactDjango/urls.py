745e43fed505ab56e556c24a45899c001b8e6bd4
Arunarchitect <intformbase@gmail.com>
2023-10-15 11:36:19 +0530

    serving media files solved deploy ready

--- ReactDjango/urls.py
+++ ReactDjango/urls.py
@@ -18,12 +18,16 @@ from django.urls import path, include
 from frontend.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("frontend.urls")),
-    path('api/', include("blog.urls"))
+    path('api/', include("blog.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from django.conf import settings
