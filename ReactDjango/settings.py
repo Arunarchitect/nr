2e40176926e857fbda3a32d830bfdc2030e44544
Arunarchitect <intformbase@gmail.com>
2023-10-15 15:01:43 +0530

    api to https

--- ReactDjango/settings.py
+++ ReactDjango/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "frontend", "frontend", "public"),
-#     os.path.join(BASE_DIR, "frontend", "frontend", "dist")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend", "frontend", "public"),
+    os.path.join(BASE_DIR, "frontend", "frontend", "dist")
+]
 
 STATIC_ROOT = BASE_DIR /"static"
 
--- frontend/frontend/src/components/Blog.jsx
+++ frontend/frontend/src/components/Blog.jsx
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
-const baseUrl = 'http://127.0.0.1:8000/api/blog/';
+
+const baseUrl = 'https://127.0.0.1:8000/api/blog/';
 
 function Blog() {
     const [person, setPerson] = useState([]);
